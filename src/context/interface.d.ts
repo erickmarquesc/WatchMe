b6f810f3414d12ef906a26102487a6351847400c
Erick Marques <erick.marquesc@hotmail.com>
2023-01-25 22:16:58 -0300

    feat: :sparkles: add interfaces

--- src/context/interface.d.ts
+++ src/context/interface.d.ts
@@ -51,3 +51,31 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
   selected: boolean;
 };
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+};
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+};
+
+export interface MovieConstextProps {
+  genres: GenreResponseProps[];
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+  selectedGenreId: number;
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>
+}
+export interface MovieContextProviderProps {
+  children: ReactNode,
+}
